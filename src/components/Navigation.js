43ec6c961e50e7b333fe714285993ea5ce552f74
RyanSWest <west56309@gmail.com>
2019-06-05 21:28:11 -0500

    links done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Navigation = () => {
   return (
@@ -6,13 +7,18 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+           <Link to ='/'>Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <a href="/about"> About         <Link to ='/about'>  </Link>
+ </a>
+          <Link to ='/about'/> 
         </div>
         <div>
-          <a href="">Contact</a>
+                   <Link to ='/contact'> Contact </Link>
+
+          <Link to ='/contact'>  </Link>
+
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+        {/* <Route path ='/'component ={Home} exact/>
+    <Route path ='/about' component = {About}/>
+    <Route path = '/contact' component ={Contact}/> */}
+    </Router>
+ , document.getElementById('root'));
